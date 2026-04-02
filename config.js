c504a90b954ec0da3099fb9e9a91970b4a3c3a77
Tom <alviolid314@gmail.com>
2026-04-02 17:28:20 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    botName: "Rahi Bot 😈",
+    ownerName: "Rahi",
+    ownerNumber: "8801XXXXXXXXX",
+    prefix: ".",
+
+    antiLink: true,
+    antiSpam: true,
+    antiDelete: true,
+
+    autoAI: true
+};
